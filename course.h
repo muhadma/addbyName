25b7a08f77a932bf9e41cde79f4c972b4eda8465
muhadma <107919937+muhadma@users.noreply.github.com>
2024-09-10 21:04:22 +0800

    Add files via upload

---
+++ course.h
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include "student.h"
+
+using namespace std;
+
+class Course {
+    Student** students;
+    string topic;
+    int size;
+    int capacity;
+    int yearlvl;
+    string program;
+    
+    public: 
+        Course(string topic) : topic(topic), size(0), yearlvl(0), program("") {
+            capacity = 5;
+            students = new Student*[capacity];  // Allocate memory using new[]
+        }
+
+        void addStudent(Student* s) {
+            if (size == capacity) {
+                int newCapacity = capacity + (capacity / 2);
+                students = (Student**)realloc(students, newCapacity * sizeof(Student*));
+                capacity = newCapacity;
+            }
+            int index = size;
+            for (int i = 0; i < size; i++) {
+                if (s->name.compare(students[i]->name) < 0) {
+                    index = i;
+                    break;
+                }
+            }
+
+            for (int j = size; j > index; j--) {
+                students[j] = students[j-1];
+            }
+
+            students[index] = s;
+            size++;
+        }
+        
+        void print() {
+            cout << "Student size: " << size << "/" << capacity << endl;
+            for (int i = 0; i < capacity; i++) {
+                if (i >= size) {
+                    cout << i + 1 << " (none) " << endl;
+                } else {
+                    cout << i + 1 << ": " << students[i]->name << " " 
+                         << students[i]->program << " " 
+                         << students[i]->year_level << endl;
+                }
+            }
+        }
+};
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "course.h"
+
+using namespace std;
+
+int main() {
+    char op;
+    int count;
+    Course* ab = new Course("DSA");
+    string name;
+    Student* s;
+    do {
+        cout << "Op: ";
+        cin >> op;
+        
+        switch(op) {
+            case 'a':
+                s = new Student();
+                cout << "Enter name: ";
+                cin >> s->name;
+                cout << "Program: ";
+                cin >> s->program;
+                cout << "Year Level: ";
+                cin >> s->year_level;
+                
+                ab->addStudent(s);
+                break;
+            case 'p':
+                ab->print();
+                break;
+            case 'x':
+                return 0;
+        }
+        
+    }while (op != 'x');
+}
